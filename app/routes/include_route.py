8fd77373008725d9a71912bac716dba04439734c
alexvieirask <alexvieiradias2019@gmail.com>
2022-10-13 08:30:14 -0300

    rota linux update

--- app/routes/include_route.py
+++ app/routes/include_route.py
@@ -16,9 +16,9 @@ from schemas.giftcard import GiftCard
     curl -H \Content-Type:application/json\ -X POST --data "{\"name\":\"Emanoela Rodrigues Erthal\",\"username\":\"manu_erthal\",\"email\":\"manu@gmail.com\",\"password\":\"manu\"}" http://localhost:5000/user/include
 
     Linux:
-    curl -H \Content-Type: application/json\ -X POST --d "{"name":"Alex Vieira Dias","username":"alexvieirasdias","email":"alexvieirasdias@gmail.com","password":"teste"}" http://localhost:5000/user/include
-
-    curl -H \Content-Type: application/json\ -X POST --d "{"name":"Emanoela Rodrigues Erthal","username":"manu_ertha","email":"manu@gmail.com","password":"manu"}" http://localhost:5000/user/include
+    curl -d '{"name":"Alex Vieira Dias","username":"alexvieiradiasSK","email":"alexvieiradias2019@gmail.com","password":"teste"}' -X  POST -H "Content-Type:application/json" localhost:5000/user/include
+    
+    curl -d '{"name":"Emanoela Rodrigues Erthal","username":"manu.erthal","email":"emanoela@gmail.com","password":"teste"}' -X  POST -H "Content-Type:application/json" localhost:5000/user/include
 
 2. Game
 
@@ -26,7 +26,7 @@ from schemas.giftcard import GiftCard
     curl -H \Content-Type:application/json\ -X POST --data "{\"title\":\"The test\",\"description\":\"this game...\",\"categorie\":\"aventura\",\"price\":\"2\",\"required_age\":\"0\",\"launch_date\":\"24/01/2005\",\"developer\":\"The Tester\"}" http://localhost:5000/game/include
 
     Linux: 
-    curl -H \Content-Type: application/json\ -X POST --d "{"title":"The test","description":"this game...","categorie":"aventura","price":"2","required_age":"0","launch_date":"24/01/2005","developer":"The Tester"}" http://localhost:5000/user/include
+    curl -d '{"title":"The Game of Year","description":"This game...","categorie":"aventura","price":"50.99","required_age":"0","launch_date":"24/01/2005","developer":"God"}' -X  POST -H "Content-Type:application/json" localhost:5000/user/include
 
 '''
 @app.route("/<string:class_type>/include", methods = ['POST'])
